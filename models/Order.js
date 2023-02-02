a325e53f23b373c3cd07c830aaa64a96a58024f8
darshak kakadiya <90410040+darshak3112@users.noreply.github.com>
2023-02-02 18:47:16 +0530

    model created

---
+++ models/Order.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const orderSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
+    address: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'Pending',
+        required: true
+    },
+
+}, { timestamps: true });
+
+export default model("Order", orderSchema);
---
+++ models/User.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+export default model("User", userSchema);
