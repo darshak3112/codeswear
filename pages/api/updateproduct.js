50c04f82986bc51503a3414a28140c1356b35812
darshak kakadiya <90410040+darshak3112@users.noreply.github.com>
2023-02-03 00:18:40 +0530

    update api

--- pages/api/updateproduct.js
+++ pages/api/updateproduct.js
@@ -4,11 +4,17 @@ import connectDB from "../../middleware/mongoose";
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
-    for (let i = 0; i < req.body.length; i++) {
-      let p = await Product.findByIdAndUpdate(req.body[i]._id, req.body[i]);
+    try {
+      for (let i = 0; i < Object.keys(req.body).length; i++) {
+        let p = await Product.findByIdAndUpdate(req.body[i]._id, req.body[i]);
+        console.log(p);
+      }
+      res.status(200).json({ success: "success" });
+    } catch (e) {
+      console.log("err : ", e);
     }
-    res.status(200).json({ success: "success" });
-  } else {
+  }
+  else {
     res.status(400).json({ error: "This method is not allowed" });
   }
 };
